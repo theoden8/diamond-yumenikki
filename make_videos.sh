71cb0326d451299b3617b787ea19ad309c28cd78
Kirill Rodriguez <theoden8@gmail.com>
2024-12-09 12:59:59 +0000

    script to make videos

---
+++ make_videos.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -ex
+
+for video_name in data/video_*/; do
+  mp4_name="${video_name%/}.mp4"
+  if test -e "$mp4_name"; then
+    continue
+  fi
+  if test $(find "$video_name" -type f -name "*.jpg" | wc -l) -gt 3; then
+    ffmpeg -framerate 8 -pattern_type glob -i "${video_name%/}/*.jpg" -c:v libopenh264 -r 24 -pix_fmt bgr24 -y "$mp4_name" \
+      -nostdin
+  fi
+done
